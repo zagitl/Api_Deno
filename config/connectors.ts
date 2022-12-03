a3741207a9a5fad9f1b574b3ce52b7ded294c1e4
zat_orcru@hotmail.com <15723342+Rebjai@users.noreply.github.com>
2022-12-03 15:23:38 -0600

    First commit

---
+++ config/connectors.ts
@@ -0,0 +1,20 @@
+
+import { MongoClient, ClientMariaDB } 
+    from "../dependences.ts";
+
+
+const mongoClient = new MongoClient();
+const URI_MONGO="mongodb://developerItvo:t0ps3cr3t@localhost:27017/quotes?authMechanism=SCRAM-SHA-256&authSource=admin";
+export const connectorMongoDB = await mongoClient.connect(URI_MONGO);
+
+const CONN_MARIADB = {
+    hostname: "localhost",
+    port: 3360,
+    username: "devDeno",
+    password: "mariadb",
+    db: "quotes",
+    poolSize: 10
+};
+
+export const connectorMariaDB = await new ClientMariaDB().connect(CONN_MARIADB);
+
